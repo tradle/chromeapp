cbe77579e2c9da522a04623ee6e35ecd8eeb084f
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-03-17 10:40:40 -0400

    hardcode public key in generated sample builds

--- make-samples.js
+++ make-samples.js
@@ -4,8 +4,9 @@ var ncp = require('ncp');
 var fs = require('fs');
 var path = require('path');
 var rimraf = require('rimraf');
+var cheerio = require('cheerio');
 
-['Jane', 'Kate'].forEach(function(name) {
+['Jane', 'Kate'].forEach(function(name, nameIdx) {
   var lower = name.toLowerCase();
   var copyPath = path.resolve('build/' + lower);
 
@@ -14,6 +15,17 @@ var rimraf = require('rimraf');
 
     ncp('app', copyPath, function(err) {
       if (err) return console.log(err);
+
+      var htmlPath = path.resolve('build/' + lower + '/index.html');
+      fs.readFile(htmlPath, function(err, html) {
+        if (err) return console.log(err);
+
+        var $ = cheerio.load(html);
+        var webview = $('webview');
+        var src = webview.attr('src');
+        webview.attr('src', src + '&-pubkey=' + (nameIdx + 1));
+        fs.writeFile(htmlPath, $.html());
+      });
 
       var mPath = path.resolve('build/' + lower + '/manifest.json');
       var manifest = require(mPath);
